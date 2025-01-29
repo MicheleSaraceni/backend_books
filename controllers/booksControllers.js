c7792b6e879de4cc1fd5ecac1fe181d108993f7f
Andrea050101 <agiancristiano90@gmail.com>
2025-01-29 16:05:09 +0100

    bookController refactoring

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -1,108 +1,97 @@
-
+//Import necessari
 import connection from "../connection.js"
 
+//Funzioni
+//Index mostra tutti i libri
 const index = (req, res) => {
-
-    const sql = "SELECT * FROM `books`"
+    //Query
+    const sql = "SELECT * FROM `books`";
+    //Esecuzione query
     connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({
-            error: "Database query failed"
-        })
+        //Se c'è un errore ritorna un error 500
+        if (err) return res.status(500).json({ error: "Database query failed" });
+        //Altrimenti ritorna tutti i libri
         res.json({
-
             length: results.length,
             items: results
+        });
+    });
+};
 
-        })
-
-    })
-
-
-}
-
-
+//Show mostra un libro in base all'id
 const show = (req, res) => {
-
-    const id = parseInt(req.params.id)
+    //recupero l'id
+    const id = parseInt(req.params.id);
+    //preparo la query
     const sql = `SELECT books.*, AVG(reviews.vote) AS vote_avarage FROM books
     LEFT JOIN reviews ON books.id = reviews.book_id
     WHERE books.id = ?`
+    //Esecuzione query
     connection.query(sql, [id], (err, results) => {
-
-        if (err)
-            return res.status(500).json({ error: err })
-
-        if (!results[0])
-            return res.status(404).json({ error: "element not found" })
-
+        //Se c'è un errore ritorna un error 500
+        if (err) return res.status(500).json({ error: err });
+        //Se l'elemento non è presente ritorna un error 404
+        if (!results[0]) return res.status(404).json({ error: "element not found" });
+        //Altrimenti ritorna l'elemento ma prima dobbiamo estrarre le sue reviews
         if (results[0]) {
+            //preparo la query
             const sql2 = `SELECT reviews.* FROM reviews
             WHERE book_id = ?`;
-            const item = results[0]
+            //salvo l'elemento nella variabile item
+            const item = results[0];
+            //Esecuzione query
             connection.query(sql2, [id], (err, results2) => {
+                //Se c'è un errore ritorna un error 500
+                if (err) return res.status(500).json({ error: err });
+                //Salvo le reviews nella variabile reviews
+                const reviews = results2;
+                //ritorno l'elemento
+                return res.json({ item: item, reviews: reviews });
+            });
+        };
+    });
+};
 
-                if (err)
-                    return res.status(500).json({ error: err })
-
-                const reviews = results2
-                return res.json({ item: item, reviews: reviews })
-
-            })
-        }
-
-    })
-
-}
-
+//Store per libri e reviews
+//Store per libri (Da modificare se implementeremo aggiunta di libri)
 const store = (req, res) => {
-    //recuper l'id
     const { id } = req.params;
-
-    //recupero il body
     const { name, text, vote } = req.body;
-
-    //preparo la query
     const sql = "INSERT INTO reviews (name, text, vote, book_id) VALUES (?, ?, ?, ?)";
-
-    //Eseguiamo la query
-    //console.log(results); // results contiene la riga che ci ritorna il server
-    //console.log(fields); // fields contiene dati extra di results, se disponibili
     connection.query(sql, [name, text, vote, id], (err, results) => {
         if (err) return res.status(500).json({ error: "Database query failed" });
         res.status(201);
         res.json({ message: "Reviews added", id: results.insertId });
     });
-}
+};
 
+//Store per reviews
 const storeReview = (req, res) => {
-
+    //recupero l'id
     const { id } = req.params;
+    //recupero i dati
     const { name, text, vote } = req.body;
-
-
+    //preparo la query
     const sql = "INSERT INTO reviews (name, text, vote, book_id) VALUES (?, ?, ?, ?)";
-
+    //Esecuzione query
     connection.query(sql, [name, text, vote, id], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: "Database query failed" });
-        } else {
-            return res.json({ message: "Reviews added", id: results.insertId });
-        }
-
+        //Se c'è un errore ritorna un error 500
+        if (err) return res.status(500).json({ error: "Database query failed" });
+        else return res.json({ message: "Reviews added", id: results.insertId });
     });
-}
+};
 
+//Destroy per eliminare un libro
 const destroy = (req, res) => {
-
-    const id = parseInt(req.params.id)
-    const sql = "DELETE FROM `books` WHERE `id` = ? "
+    //recupero l'id
+    const id = parseInt(req.params.id);
+    //preparo la query
+    const sql = "DELETE FROM `books` WHERE `id` = ? ";
+    //Esecuzione query
     connection.query(sql, [id], () => {
+        return res.json({ message: "element deleted" });
+    });
+};
 
-        return res.json({ message: "element deleted" })
-
-
-    })
-
-}
-
+//esporto le funzioni
 export { index, show, store, storeReview, destroy } 
